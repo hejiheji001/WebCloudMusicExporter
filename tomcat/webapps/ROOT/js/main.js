47e46d3925c6472fc2de8675ac9383a7f70bf588
hejiheji001@gmail.com <hejiheji001@gmail.com>
2016-03-18 19:15:25 +0000

    Export

--- tomcat/webapps/ROOT/js/main.js
+++ tomcat/webapps/ROOT/js/main.js
@@ -6,6 +6,7 @@
 	var input = $("#input");
 	var songlist = $("#songlist");
 	var result = $("#result");
+	var exp = $("#export");
 
 	auto.on({
 		click: function(e){
@@ -31,7 +32,39 @@
                 goMethod("auto");
             }
         }
-    })
+    });
+
+	exp.on({
+		click: function(e){
+			exportList();
+		}
+	});
+
+	var exportList = function(){
+		var a = $("[name='aria2c']");
+		var w = $("[name='wget']");
+		var c = $("[name='curl']");
+		var aList = [];
+		var wList = [];
+		var cList = [];
+		if(songs){
+			var num = songs.length;
+			for (var i = 0; i < num; i++) {
+				var s = songs[i];
+				var d = s["durl"];
+				var n = s["name"];
+				aList.push("aria2c -c -s10 -k1M -x10 -o \"" + n + "\" --header \"Referer: http://music.163.com\" \"" + d + "\"");
+				wList.push("wget -o \"" + n + "\" --referer=http://music.163.com \"" + d + "\"");
+				cList.push("curl -o \"" + n + "\" -e http://music.163.com \"" + d + "\"");
+			}
+			aList.push("\r\n");
+			wList.push("\r\n");
+			cList.push("\r\n");
+		}
+		a.text(aList.join("\r\n"));
+		w.text(wList.join("\r\n"));
+		c.text(cList.join("\r\n"));
+	}
 
 	var goMethod = function(method){
 		songlist.slideUp();
@@ -61,9 +94,10 @@
 	};
 
 	var getInfo = function(u){
-		var str =  '<a href="${durl}" download="${name}" target="_blank" class="list-group-item list-group-item-info songItem">\
-						<h4 class="list-group-item-heading text-center">${name}</h4>\
-					</a>';
+		var str =  '<div class="list-group-item list-group-item-info text-center list-inline">\
+						<a href="${durl}" download="${name}" target="_blank" class="songItem" style="font-size: 16px;font-weight: 500">${name}</a>\
+						<a class="btn btn-default" style="right: 15px;position: absolute;padding: 0px 12px;">Save To Pan</a>\
+					</div>';
         $(".songItem").remove();
 		$.ajax({
 			method: "GET",
@@ -71,7 +105,7 @@
 			url: u,
 			success : function (data) {
 				var num = data.songs.length;
-				var songs = data.songs;
+				window.songs = data.songs;
 				var res = data.result;
 				var list = data.listname;
 				var dom = [];
@@ -88,7 +122,7 @@
 						var s = songs[i];
 						var d = s["durl"];
 						var n = s["name"];
-                        console.log(s);
+                        //console.log(s);
 						tem = str.replace("${durl}", d);
 						tem = tem.replace(/\$\{name\}/g, n);
 						dom.push(tem);
